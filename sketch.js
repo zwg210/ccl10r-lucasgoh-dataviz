4b84c109ac11ffce707e04a688162745e6513156
zwg210 <zwg210@nyu.edu>
2021-04-16 11:00:59 +0800

    sketched lines with x,y from bodypoints

--- sketch.js
+++ sketch.js
@@ -1,7 +1,34 @@
+let data;
+
+function preload() {
+  data = loadJSON("pose.json");
+}
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for (let key in data) {
+    console.log(key);
+    console.log(data[key]);
+  }
 }
+
+
 
 function draw() {
   background(200);
+  let px = null;
+  let py = null;
+  fill(255, 0, 0);
+  let n = data["bodypoints"]
+  for (let key in n) {
+    let x = n[key].x;
+    let y = n[key].y;
+    if (px != null && py != null) {
+      line(px, py, x, y)
+    }
+    circle(x, y, 10);
+    px = n[key].x;
+    py = n[key].y;
+  }
 }
